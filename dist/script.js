886b4d5fd50d68b6ae9206ffd5f8a18f161931bb
Gerard Juan <gerardandrejuan@gmail.com>
2024-02-09 15:34:05 -0500

    resolve bug, testing 100vh mobile menu

--- dist/script.js
+++ dist/script.js
@@ -200,5 +200,5 @@ setupCarousel();
 
 function Menu(e) {
 	let list = document.querySelector('ul');
-	e.name === 'menu' ? ((e.name = 'close'), list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : ((e.name = 'menu'), list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'));
+	e.name === 'menu' ? ((e.name = 'close'), list.classList.add('opacity-100')) : ((e.name = 'menu'), list.classList.remove('opacity-100'));
 }
